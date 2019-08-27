4822aa150a9269df11fc7b45b4792bdd914d5109
jmechnich <joerg.mechnich@gmail.com>
2019-08-27 16:42:14 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
+PYTHON=python3
+
 PREFIX=/usr/local
 #PREFIX=/usr
 FILES=files.txt
 
+if ! $PYTHON -c 'import PyQt4' 2>/dev/null; then
+    echo "Installing ${PYTHON}-pyqt4"
+    sudo apt-get install ${PYTHON}-pyqt4
+fi
+
+if ! $PYTHON -c 'import PyQt4' 2>/dev/null; then
+    echo "Installing ${PYTHON}-pyqt4"
+    sudo apt-get install ${PYTHON}-pyqt4
+fi
+
 echo "Installing to $PREFIX, keeping list of files in $FILES"
 echo
 
-sudo python setup.py install --prefix "$PREFIX" --record "$FILES"
+sudo $PYTHON setup.py install --prefix "$PREFIX" --record "$FILES"
 
 echo
 echo "Uninstall with 'cat $FILES | sudo xargs rm -rf'"
