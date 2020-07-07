7e3f056f891978262123549c4cd8cbea863a6e69
jmechnich <joerg.mechnich@gmail.com>
2020-07-07 15:19:53 +0200

    use setuptools instead of distutils, updated package information

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+import setuptools
 
-setup(name='appletlib',
-      version='1.0',
-      description='',
-      author='Joerg Mechnich',
-      author_email='joerg.mechnich@gmail.com',
-      url='https://github.com/jmechnich/appletlib',
-      packages=['appletlib'],
-     )
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='appletlib',
+    version='1.0.0',
+    author='Joerg Mechnich',
+    author_email='joerg.mechnich@gmail.com',
+    description='Python library providing a QSystemTrayIcon wrapper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/jmechnich/appletlib',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    python_requires='>=3.6',
+)
